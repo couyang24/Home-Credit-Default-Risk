0f2dc236c789f0828c55c95c3d534c06e831eb78
Chengran Ouyang <30837746+couyang24@users.noreply.github.com>
2018-05-17 22:38:57 -0400

    svm benchmark

---
+++ SVM benchmark.R
@@ -0,0 +1,77 @@
+library(tidyverse)
+library(data.table)
+library(skimr)
+library(DT)
+library(plotly)
+
+<img src="https://i.imgur.com/O6QrzdG.jpg">
+
+train <-fread('application_train.csv', stringsAsFactors = FALSE, showProgress=F,
+              data.table = F, na.strings=c("NA","NaN","?", ""))
+test <-fread('application_test.csv', stringsAsFactors = FALSE, showProgress=F,
+              data.table = F, na.strings=c("NA","NaN","?", ""))
+full <- bind_rows(train,test)
+
+
+
+setdiff(names(train),names(test))
+
+train %>% 
+  count(TARGET) %>% 
+  plot_ly(labels = ~TARGET, values = ~n, type = 'pie') %>%
+  layout(title = 'Target Variable Distribution',
+         xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+         yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+
+
+
+full %>% skim() %>% kable()
+
+full[sample(1:nrow(full),size = 1000),] %>% 
+  datatable(filter = 'top', options = list(
+    pageLength = 15, autoWidth = TRUE
+  ))
+
+
+
+Target <- train$TARGET
+Id <- test$SK_ID_CURR
+
+
+
+full$TARGET %>% table()
+
+
+full[,c('SK_ID_CURR','TARGET')] <- NULL
+rm(train, test)
+
+chr <- full[,sapply(full,is.character)]
+int <- full[,sapply(full,is.numeric)]
+
+fill_chr <- function(df){
+  for(i in 1:ncol(df)){
+    for(j in 1:nrow(df)){
+      if(is.na(df[j,i])){
+        df[j,i] = "Not Avaiable"
+      }
+    } 
+  } 
+  return(df)
+}
+chr <- fill_chr(chr)
+fac <- chr %>% lapply(as.factor) %>% as_data_frame()
+
+full <- bind_cols(fac,int)
+
+micemod <- full %>% mice(method='rf')
+full <- complete(micemod)
+
+rm(chr,fac,int,fill_chr,micemod)
+
+train <- full[1:length(Target),]
+test<-full[(length(Target)+1):nrow(full),]
+
+svm_model<-svm(Target~., data=train, cost = 3)
+svm_pred <- predict(svm_model,newdata = test)
+solution <- data.frame(SK_ID_CURR=Id,TARGET=svm_pred)
+write.csv(solution,"svm_benchmark_solution.csv",row.names = F)
